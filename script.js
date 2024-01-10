8037ae3aefa191f78f1a00624b9f44963c7fe33f
Angeline <146100322+Angelinelmb@users.noreply.github.com>
2024-01-11 00:52:46 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    var downloadSection = document.getElementById("downloadSection");
+    downloadSection.style.display = (downloadSection.style.display === "none") ? "block" : "none";
+}
+function goBack() {
+    window.history.back();
+}
