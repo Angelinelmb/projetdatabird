19fc0b8d7e3e66e58db9728402637b718caf48b0
Angeline <146100322+Angelinelmb@users.noreply.github.com>
2024-01-11 00:57:27 +0100

    Update Bird.js

--- Bird.js
+++ Bird.js
@@ -1,122 +1,40 @@
-var SCREEN_WIDTH = window.innerWidth,
-SCREEN_HEIGHT = 700,
-SCREEN_WIDTH_HALF = SCREEN_WIDTH  / 2,
-SCREEN_HEIGHT_HALF = SCREEN_HEIGHT / 2;
+var Bird = function () {
 
-var camera, scene, renderer,
-birds, bird;
+	var scope = this;
 
-var boid, boids;
+	THREE.Geometry.call( this );
 
-var stats;
+	v(   5,   0,   0 );
+	v( - 5, - 2,   1 );
+	v( - 5,   0,   0 );
+	v( - 5, - 2, - 1 );
 
-init();
-animate();
+	v(   0,   2, - 6 );
+	v(   0,   2,   6 );
+	v(   2,   0,   0 );
+	v( - 3,   0,   0 );
 
-function init() {
+	f3( 0, 2, 1 );
+	// f3( 0, 3, 2 );
 
-    camera = new THREE.PerspectiveCamera( 75, SCREEN_WIDTH / SCREEN_HEIGHT, 1, 10000 );
-    camera.position.z = 450;
+	f3( 4, 7, 6 );
+	f3( 5, 6, 7 );
 
-    scene = new THREE.Scene();
-    scene.background = new THREE.Color( 0xeeeeee );
+	this.computeFaceNormals();
 
-    birds = [];
-    boids = [];
+	function v( x, y, z ) {
 
-    for ( var i = 0; i < 200; i ++ ) {
+		scope.vertices.push( new THREE.Vector3( x, y, z ) );
 
-        boid = boids[ i ] = new Boid();
-        boid.position.x = Math.random() * 400 - 200;
-        boid.position.y = Math.random() * 400 - 200;
-        boid.position.z = Math.random() * 400 - 200;
-        boid.velocity.x = Math.random() * 2 - 1;
-        boid.velocity.y = Math.random() * 2 - 1;
-        boid.velocity.z = Math.random() * 2 - 1;
-        boid.setAvoidWalls( true );
-        boid.setWorldSize( 500, 500, 400 );
+	}
 
-        bird = birds[ i ] = new THREE.Mesh( new Bird(), new THREE.MeshBasicMaterial( { color:Math.random() * 0xffffff, side: THREE.DoubleSide } ) );
-        bird.phase = Math.floor( Math.random() * 62.83 );
-        scene.add( bird );
+	function f3( a, b, c ) {
 
+		scope.faces.push( new THREE.Face3( a, b, c ) );
 
-    }
-
-    renderer = new THREE.CanvasRenderer();
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( SCREEN_WIDTH, SCREEN_HEIGHT );
-
-    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-    document.getElementById('oiseaux').appendChild( renderer.domElement );
-
-    stats = new Stats();
-    // document.getElementById( 'container' ).appendChild(stats.dom);
-
-    //
-
-    window.addEventListener( 'resize', onWindowResize, false );
+	}
 
 }
 
-function onWindowResize() {
-
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize( window.innerWidth, window.innerHeight );
-
-}
-
-function onDocumentMouseMove( event ) {
-
-    var vector = new THREE.Vector3( event.clientX - SCREEN_WIDTH_HALF, - event.clientY + SCREEN_HEIGHT_HALF, 0 );
-
-    for ( var i = 0, il = boids.length; i < il; i++ ) {
-
-        boid = boids[ i ];
-
-        vector.z = boid.position.z;
-
-        boid.repulse( vector );
-
-    }
-
-}
-
-//
-
-function animate() {
-
-    requestAnimationFrame( animate );
-
-    stats.begin();
-    render();
-    stats.end();
-
-}
-
-function render() {
-
-    for ( var i = 0, il = birds.length; i < il; i++ ) {
-
-        boid = boids[ i ];
-        boid.run( boids );
-
-        bird = birds[ i ];
-        bird.position.copy( boids[ i ].position );
-
-        var color = bird.material.color;
-        color.r = color.g = color.b = ( 500 - bird.position.z ) / 1000;
-
-        bird.rotation.y = Math.atan2( - boid.velocity.z, boid.velocity.x );
-        bird.rotation.z = Math.asin( boid.velocity.y / boid.velocity.length() );
-
-        bird.phase = ( bird.phase + ( Math.max( 0, bird.rotation.z ) + 0.1 )  ) % 62.83;
-        bird.geometry.vertices[ 5 ].y = bird.geometry.vertices[ 4 ].y = Math.sin( bird.phase ) * 5;
-
-    }
-
-    renderer.render( scene, camera );
-
-}
+Bird.prototype = Object.create( THREE.Geometry.prototype );
+Bird.prototype.constructor = Bird;
